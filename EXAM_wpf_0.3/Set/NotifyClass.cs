ec3afd6dd65babd42f356b390bec537f080b2853
Roman <yozhik13@gmail.com>
2023-01-05 19:09:56 +0300

    Добавьте файлы проекта.

---
+++ EXAM_wpf_0.3/Set/NotifyClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXAM_wpf_0._3.Set
+{
+    public class NotifyClass : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;   //событие публичное
+        public virtual void OnPropertyChanged([CallerMemberName] string property = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
